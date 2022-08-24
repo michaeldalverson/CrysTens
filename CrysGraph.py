369ccda01289379b85abc8c32180fcd553f06985
Michael Alverson <michaeldavidalverson@gmail.com>
2022-08-24 14:10:35 -0700

    Prelimary code for getting a CIF from a CrysGraph.

--- CrysGraph.py
+++ CrysGraph.py
@@ -45,6 +45,7 @@ class CrysGraph:
                         'length': [0.26288351681625777, 0.646245505143038, 0.4067858340586399, 2.15365474510736]}
 
     self.attribute_list = ["atom", "x", "y", "z", "a", "b", "c", "alpha", "beta", "gamma", "sg"]
+    self.parameter_list = ["a", "b", "c", "alpha", "beta", "gamma", "sg"]
 
 
 
@@ -154,7 +155,6 @@ class CrysGraph:
           self.crysgraph[12 + adj_idx, 12 + site_idx, 1:] += self.normalize_avg_std["dir"][2]
           self.crysgraph[12 + adj_idx, 12 + site_idx, 1:] /= self.normalize_avg_std["dir"][2] + self.normalize_avg_std["dir"][3]
 
-
   def unnormalize_crysgraph(self, method: str = "divide_near_max"):
     if not self.normalized:
       return
@@ -217,6 +217,113 @@ class CrysGraph:
           self.crysgraph[12 + adj_idx, 12 + site_idx, 1:] *= self.normalize_avg_std["dir"][1]
           self.crysgraph[12 + adj_idx, 12 + site_idx, 1:] += self.normalize_avg_std["dir"][0]
           self.crysgraph[12 + adj_idx, 12 + site_idx, 1:] -= self.direction_fix
+
+  def get_cif(self, file_path, ref_percent: float = 0.2, rel_coord_percent: float = None, num_coord_clusters: int = None, num_atom_clusters: int = 3, generator: str = "Real CIF"):
+    crystal_cif = {}
+    ref_angle = self.crys_graph[12, 7, 0]
+    ref_avg = ref_angle
+    for num in range(13, self.crys_graph.shape[0]):
+      if abs(self.crys_graph[num, 7, 0] - ref_avg)/ref_avg >= ref_percent:
+        break
+      else:
+        ref_angle += self.crys_graph[num, 7, 0]
+        reference_average = ref_angle / (num - 11)
+    
+    constrained_crysgraph = self.crysgraph[:num, :num, :]
+    for i in range(12, constrained_crysgraph.shape[0]):
+      for j in range(12):
+        avg_val = (np.sum(constrained_crysgraph[i, j, :]) + np.sum(constrained_crysgraph[j, i, :]))/(2 * constrained_crysgraph.shape[2]) 
+        constrained_crysgraph[i, j, :], constrained_crysgraph[j, i, :] = avg_val, avg_val
+
+    for i in range(4, 11):
+      sum_val = 0
+      for j in range(12, constrained_crysgraph.shape[0]):
+        sum_val += np.sum(constrained_crysgraph[i, j, :]) + np.sum(constrained_crysgraph[j, i, :])
+      avg_val = sum_val / ((constrained_crysgraph.shape[0] - 12) * constrained_crysgraph.shape[2])
+      constrained_crysgraph[i, 12:, :], constrained_crysgraph[12:, i, :] = avg_val, avg_val
+      crystal_cif[self.parameter_list[i - 4]] = avg_val
+
+    for i in range(12, constrained_crysgraph.shape[0]):
+      for j in range(12, constrained_crysgraph.shape[0]):
+        avg_val = (constrained_crysgraph[i, j, 0] + constrained_crysgraph[j, i, 0])/2
+        constrained_crysgraph[i, j, 0], constrained_crysgraph[j, i, 0] = avg_val, avg_val
+        for k in range(1, 4):
+          if constrained_crysgraph[i, j, k] > 0:
+            avg_val = (constrained_crysgraph[i, j, k] + abs(constrained_crysgraph[j, i, k]))/2
+            constrained_crysgraph[i, j, k], constrained_crysgraph[j, i, k] = avg_val, -avg_val
+          else:
+            avg_val = (abs(constrained_crysgraph[i, j, k]) + constrained_crysgraph[j, i, k])/2
+            constrained_crysgraph[i, j, k], constrained_crysgraph[j, i, k] = -avg_val, avg_val
+
+    crystal_cif["site_list"] = {}
+    for i in range(12, constrained_crysgraph.shape[0]):
+      crystal_cif["site_list"][i - 12] = {}
+      crystal_cif["site_list"][i - 12]["atom"] = constrained_crysgraph[i, 0, 0]
+      crystal_cif["site_list"][i - 12]["x"] = constrained_crysgraph[i, 1, 0]
+      crystal_cif["site_list"][i - 12]["y"] = constrained_crysgraph[i, 2, 0]
+      crystal_cif["site_list"][i - 12]["z"] = constrained_crysgraph[i, 3, 0]
+
+      crystal_cif["site_list"][i - 12]["adj_list"] = []
+      for j in range(12, constrained_crysgraph.shape[0]):
+        adj_x = crystal_cif["site_list"][i - 12]["x"] - constrained_crysgraph[i, j, 1]
+        adj_y = crystal_cif["site_list"][i - 12]["y"] - constrained_crysgraph[i, j, 2]
+        adj_z = crystal_cif["site_list"][i - 12]["z"] - constrained_crysgraph[i, j, 3]
+        crystal_cif["site_list"][i - 12]["adj_list"].append((adj_x, adj_y, adj_z))
+
+    site_list = []
+    atom_list = []
+    if rel_coord_percent is None:
+      rel_coord_percent = 1 - (1/constrained_crysgraph.shape[0])
+    for site_idx in crystal_cif["site_list"]:
+      site = crystal_cif["site_list"][site_idx]
+      site_x = site["x"]
+      site_y = site["y"]
+      site_z = site["z"]
+      adj_x_list = []
+      adj_y_list = []
+      adj_z_list = []
+      adj_coord_list =site["adj_list"]
+      for adj_idx in range(len(adj_coord_list)):
+        adj_coord = adj_coord_list[adj_idx]
+        adj_x_list.append(adj_coord[0])
+        adj_y_list.append(adj_coord[1])
+        adj_z_list.append(adj_coord[2])
+      
+      site_coord_percent = 1 - rel_coord_percent
+      x_rel = site_coord_percent*site_x + rel_coord_percent*np.average(adj_x_list)
+      y_rel = site_coord_percent*site_y + rel_coord_percent*np.average(adj_y_list)
+      z_rel = site_coord_percent*site_z + rel_coord_percent*np.average(adj_z_list)
+      atom_list.append(np.around(site["atom"]))
+      site_list.append((x_rel, y_rel, z_rel))
+
+    kmeans_coord_list = []
+    for i in range(len(site_list)):
+      for j in range(3):
+        kmeans_coord_list.append(site_list[i][j])
+
+    if num_coord_clusters is None:
+      num_coord_clusters = len(kmeans_coord_list)
+
+    kmeans_coord = KMeans(n_clusters = num_coord_clusters).fit(np.array(kmeans_coord_list).reshape(-1, 1))
+    for i in range(len(kmeans_coord_list)):
+      kmeans_coord_list[i] = kmeans_coord.cluster_centers_[kmeans_coord.labels_[i]][0]
+    
+    for i in range(0, len(kmeans_coord_list), 3):
+      site_list[i//3] = [kmeans_coord_list[i], kmeans_coord_list[i + 1], kmeans_coord_list[i + 2]]
+
+    #TODO Finish PotScoring
+
+    num_atom_clusters = min(num_atom_clusters, len(atom_list))
+    kmeans_atom = KMeans(n_clusters=int(num_atom_clusters)).fit(np.array(atom_list).reshape(-1, 1))
+
+    for i in range(len(atom_list)):
+      atom_list[i] = np.around(kmeans_atom.cluster_centers_[kmeans_atom.labels_[i]])[0]
+
+    lattice = Lattice.from_parameters(a = crystal_cif["a"], b = crystal_cif["b"], c = crystal_cif["c"], alpha = crystal_cif["alpha"], beta = crystal_cif["beta"], gamma = crystal_cif["gamma"])
+    struct = Structure(lattice = lattice, species = atom_list, coords = site_list, to_unit_cell=True)
+    written_cif = str(CifWriter(struct))
+    with open(file_path, "w") as file:
+      file.write("Generated by: " + generator + "\n" + "Num unique sites: " + str(num_coord_clusters) + "\n" + "Num unique elements: " + str(num_atom_clusters) + "\n\n" + written_cif)
 
   def construct_crysgraph(self):
     self.crysgraph = np.zeros((64, 64, 4))
